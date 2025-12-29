c910d407729fad1614133338eaa339e518c425db
sammed1202 <sammedgane1008@gmail.com>
2025-12-29 23:41:21 +0530

    Delete server directory

--- server/models/SpamUser.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const spamSchema=new mongoose.Schema({
-    email:{
-        type:String,
-        unique:true,
-    }
-})
-const SpamUser=mongoose.model('SpamUser',spamSchema);
-export {SpamUser};
--- server/routes/ContactRouter.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import  {getFeedbacks, saveContact}  from '../controller/ContactController.js';
-const contactRouter=express.Router();
-contactRouter.post('/',saveContact);
-contactRouter.get('/feedback',getFeedbacks);
-export default contactRouter;
