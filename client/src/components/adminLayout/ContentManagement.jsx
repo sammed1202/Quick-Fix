f0eb4ecd92deba79437bb24877cf19ef80ff1eec
sammed1202 <sammedgane1008@gmail.com>
2025-12-29 23:41:01 +0530

    Delete client directory

--- client/src/components/adminLayout/ContentManagement.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ContentManagement = () => {
-  return (
-    <div><h1>ContentManagement</h1></div>
-  )
-}
-
-export default ContentManagement
--- client/src/components/guestLayout/GuestHeader.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import GuestNavbar from './GuestNavbar'
-
-export const GuestHeader = () => {
-  return (
-    <div><GuestNavbar/></div>
-  )
-}
